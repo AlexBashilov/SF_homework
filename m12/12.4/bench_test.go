a2c8f2507e7d3b1eed895d237482cc74013f35b1
AlexBashilov <alexrasdva91@gmail.com>
2023-08-18 22:43:57 +0000

    add HW12

--- m12/12.4/bench_test.go
+++ m12/12.4/bench_test.go
@@ -111,6 +111,35 @@ func BenchmarkSelectionSort(b *testing.B) {
 			b.StopTimer()
 		}
 	})
+	b.Run("Max - 3, size -15", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(3, 15)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("Max - 2, size - 525", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(2, 525)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("Max - 65432, size - 100500", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(65432, 100500)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
 }
 
 func BenchmarkInsertionSort(b *testing.B) {
@@ -141,6 +170,35 @@ func BenchmarkInsertionSort(b *testing.B) {
 			ar := generateSlice(10000, 100000)
 			b.StartTimer()
 			insertionSort(ar)
+			b.StopTimer()
+		}
+	})
+	b.Run("Max - 3, size -15", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(3, 15)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("Max - 2, size - 525", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(2, 525)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("Max - 65432, size - 100500", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(65432, 100500)
+			b.StartTimer()
+			bubbleSort(ar)
 			b.StopTimer()
 		}
 	})
@@ -178,6 +236,35 @@ func BenchmarkMergeSort(b *testing.B) {
 			b.StopTimer()
 		}
 	})
+	b.Run("Max - 3, size -15", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(3, 15)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("Max - 2, size - 525", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(2, 525)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("Max - 65432, size - 100500", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(65432, 100500)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
 }
 
 
@@ -209,6 +296,35 @@ func BenchmarkQuickSort(b *testing.B) {
 			ar := generateSlice(10000, 100000)
 			b.StartTimer()
 			quickSort(ar)
+			b.StopTimer()
+		}
+	})
+	b.Run("Max - 3, size -15", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(3, 15)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("Max - 2, size - 525", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(2, 525)
+			b.StartTimer()
+			bubbleSort(ar)
+			b.StopTimer()
+		}
+	})
+
+	b.Run("Max - 65432, size - 100500", func(b *testing.B) {
+		b.StopTimer()
+		for i := 0; i < b.N; i++ {
+			ar := generateSlice(65432, 100500)
+			b.StartTimer()
+			bubbleSort(ar)
 			b.StopTimer()
 		}
 	})
